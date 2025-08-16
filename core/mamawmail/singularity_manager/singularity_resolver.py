3591cb7575cf53489cd8b47b7872099a61b4e02e
Juan Carlos Ayeng <68210273+juancarlosayeng@users.noreply.github.com>
2025-08-16 20:57:32 +0800

    Create singularity_resolver.py

---
+++ core/mamawmail/singularity_manager/singularity_resolver.py
@@ -0,0 +1,21 @@
+'''
+Module: Singularity Resolver
+----------------------------
+
+Decides actions when singularity events occur.  
+
+Responsibilities:
+    - Manage conflicts at swarm scale
+    - Resolve duplication or deadlocks
+    - Restore propagation balance
+    - Coordinate with topology layer
+
+Usage:
+    Triggered by `convergence_detector.py`.  
+
+Author: Engr. Juan Carlos G. Ayeng  
+Version: 0.1.0  
+License: MIT License  
+Last updated: Aug 16, 2025  
+
+'''
