a83bb7b61b0d87978eef6bdb4c6ed4dbc9579137
Juan Carlos Ayeng <68210273+juancarlosayeng@users.noreply.github.com>
2025-08-16 20:58:59 +0800

    Update metadata_scrubber.py

--- core/mamawmail/privacy/metadata_scrubber.py
+++ core/mamawmail/privacy/metadata_scrubber.py
@@ -1,1 +1,21 @@
+'''
+Module: Metadata Scrubber
+-------------------------
 
+Removes identifying metadata from packets.  
+
+Responsibilities:
+    - Strip headers of unnecessary IDs
+    - Obfuscate routing footprints
+    - Prevent traffic analysis
+    - Work with encryption layer
+
+Usage:
+    Called after packet construction.  
+
+Author: Engr. Juan Carlos G. Ayeng  
+Version: 0.1.0  
+License: MIT License  
+Last updated: Aug 16, 2025  
+
+'''
