88f8bf659cf55f3351695d4e993b65c356185e90
Juan Carlos Ayeng <68210273+juancarlosayeng@users.noreply.github.com>
2025-08-16 20:59:45 +0800

    Update key_rotation.py

--- core/mamawmail/privacy/key_rotation.py
+++ core/mamawmail/privacy/key_rotation.py
@@ -1,1 +1,21 @@
+'''
+Module: Key Rotation Manager
+----------------------------
 
+Performs periodic key regeneration for swarm security.  
+
+Responsibilities:
+    - Rotate encryption keys regularly
+    - Distribute new keys securely
+    - Manage key lifecycle
+    - Prevent long-term compromise
+
+Usage:
+    Supports `encryption_manager.py`.  
+
+Author: Engr. Juan Carlos G. Ayeng  
+Version: 0.1.0  
+License: MIT License  
+Last updated: Aug 16, 2025  
+
+'''
