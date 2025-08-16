18ecf45d30dce098393db8b2b4e228165cc865c8
Juan Carlos Ayeng <68210273+juancarlosayeng@users.noreply.github.com>
2025-08-16 20:56:35 +0800

    Update topology_history.py

--- core/mamawmail/adaptive_topology/topology_history.py
+++ core/mamawmail/adaptive_topology/topology_history.py
@@ -1,1 +1,21 @@
+'''
+Module: Topology History
+------------------------
 
+Tracks historical changes in swarm topology.  
+
+Responsibilities:
+    - Record snapshots of swarm graphs
+    - Maintain time-series database
+    - Provide data for learning engine
+    - Enable debugging of propagation outcomes
+
+Usage:
+    Works with `topology_optimizer.py` and `learning_manager.py`.  
+
+Author: Engr. Juan Carlos G. Ayeng  
+Version: 0.1.0  
+License: MIT License  
+Last updated: Aug 16, 2025  
+
+'''
