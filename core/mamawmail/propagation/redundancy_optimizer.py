4648d587d17d8847eb72092f855bba448c8ad59a
Juan Carlos Ayeng <68210273+juancarlosayeng@users.noreply.github.com>
2025-08-16 20:53:01 +0800

    Update redundancy_optimizer.py

--- core/mamawmail/propagation/redundancy_optimizer.py
+++ core/mamawmail/propagation/redundancy_optimizer.py
@@ -1,1 +1,21 @@
+'''
+Module: Redundancy Optimizer
+----------------------------
 
+Balances exponential propagation with resource efficiency.  
+
+Responsibilities:
+    - Prevent over-replication
+    - Select optimal redundancy factor
+    - Coordinate deletions post-delivery
+    - Support energy and bandwidth conservation
+
+Usage:
+    Invoked by `fractal_propagator.py`.  
+
+Author: Engr. Juan Carlos G. Ayeng  
+Version: 0.1.0  
+License: MIT License  
+Last updated: Aug 16, 2025  
+
+'''
