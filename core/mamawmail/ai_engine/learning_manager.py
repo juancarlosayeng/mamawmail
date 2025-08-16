5fe8a111d6042ea12abab1b6a05a0b6fc75b0ce8
Juan Carlos Ayeng <68210273+juancarlosayeng@users.noreply.github.com>
2025-08-16 20:55:22 +0800

    Update learning_manager.py

--- core/mamawmail/ai_engine/learning_manager.py
+++ core/mamawmail/ai_engine/learning_manager.py
@@ -1,1 +1,21 @@
+'''
+Module: Learning Manager
+------------------------
 
+Manages retraining of ML models and dataset curation.  
+
+Responsibilities:
+    - Collect new data from swarm
+    - Retrain models periodically
+    - Deploy updated models
+    - Provide explainability hooks
+
+Usage:
+    Supports `relay_score_predictor.py` and `anomaly_detector.py`.  
+
+Author: Engr. Juan Carlos G. Ayeng  
+Version: 0.1.0  
+License: MIT License  
+Last updated: Aug 16, 2025  
+
+'''
